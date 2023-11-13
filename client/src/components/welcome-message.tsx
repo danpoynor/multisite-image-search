1b36e29879e9c01134a426210bc23c865fc62108
Dan Poynor <danpoynor@gmail.com>
2023-11-13 00:04:23 -0600

    Add proof-of-concept note to welcome message

--- client/src/components/welcome-message.tsx
+++ client/src/components/welcome-message.tsx
@@ -1,5 +1,8 @@
 export default function WelcomeMessage() {
   return (
-    <h3 className="mt-32 text-2xl font-bold">Welcome!<br />Search for images on the left.</h3>
+    <>
+      <h3 className="mt-32 text-2xl font-bold">Welcome!<br />Search for images on the left.</h3>
+      <p className="mt-4">NOTE: This site is a proof-of-concept demo site!<br />Beware there are bugs and everything is not 100% working yet.</p>
+    </>
   );
 }
