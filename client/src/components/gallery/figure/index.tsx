57ac773b9d0de0c08df07b08a8274f2d0713e2fc
Dan Poynor <danpoynor@gmail.com>
2023-11-13 00:24:49 -0600

    Reduce height of photos

--- client/src/components/gallery/figure/index.tsx
+++ client/src/components/gallery/figure/index.tsx
@@ -3,7 +3,7 @@ import Content from './content'
 
 export default function Figure({ image }) {
   return (
-    <figure className="group/item relative m-0.5 h-72 flex-[1_1_auto]">
+    <figure className="group/item relative m-0.5 h-48 flex-[1_1_auto]">
       <ToggleFavorite />
       <Content image={image} />
     </figure>
